52c3146bc00a67af5af3d0c250f725729f570db7
tomasmck <mckinless@gmail.com>
2016-03-09 01:01:50 +1100

    Refactoring and formatting

--- src/dateCalculator.js
+++ src/dateCalculator.js
@@ -1,5 +1,46 @@
 var months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+var getNumberOfFullDaysBetweenDates = function (startDate, endDate) {
+    startDate = startDate.split("/");
+    endDate = endDate.split("/");
+    if (isStartDateBeforeEndDate(startDate, endDate)) {
+        var temp = startDate;
+        startDate = endDate;
+        endDate = temp;
+    }
+    var totalDays = calculateNumberOfFullDays(startDate, endDate);
+    if (totalDays != 0) { totalDays-- }
+    return totalDays;
+};
+
+function calculateNumberOfFullDays(startDate, endDate) {
+    var startDay = parseInt(startDate[0])
+        , startMonth = parseInt(startDate[1])
+        , startYear = parseInt(startDate[2])
+        , endDay = parseInt(endDate[0])
+        , endMonth = parseInt(endDate[1])
+        , endYear = parseInt(endDate[2])
+        , yearDiff = endYear - startYear
+        , monthDiff = (endMonth - startMonth) + (yearDiff * 12)
+        , dayDiff = endDay - startDay
+        , totalDays = 0;
+
+    var numberOfFullYears = yearDiff === 0 || yearDiff === 1 ? 0 : yearDiff - 1;
+    var numberOfFullMonths = endMonth < startMonth ? 12 - (startMonth - endMonth) : monthDiff;
+    if (monthDiff < 1) {
+        totalDays = dayDiff
+    } else {
+        totalDays += addPartialMonths(startMonth, startDay, endDay);
+        totalDays += addFullMonths(startMonth, numberOfFullMonths, startYear);
+        totalDays += addFullYears(startYear, numberOfFullYears);
+    }
+    return totalDays;
+}
+
+function addPartialMonths(startMonth, startDay, endDay) {
+    return (getNumberOfDaysInMonth(startMonth) - startDay) + (endDay);
+}
+
 function addFullMonths(startMonth, fullMonths, startYear) {
     var numberOfDays = 0;
     for (var i = startMonth + 1; i < (startMonth + fullMonths); i++) {
@@ -17,9 +58,6 @@ function addFullMonths(startMonth, fullMonths, startYear) {
     return numberOfDays;
 }
 
-function getNumberOfDaysFromStartAndEndMonths(startMonth, startDay, endDay) {
-    return (getNumberOfDaysInMonth(startMonth) - startDay) + (endDay);
-}
 function addFullYears(startYear, fullYears) {
     var numberOfDays = 0;
     var firstFullYear = (startYear + 1);
@@ -33,50 +71,14 @@ function addFullYears(startYear, fullYears) {
     numberOfDays += (fullYears * 365);
     return numberOfDays;
 }
-function calulateNumberOfFullDays(startDate, endDate) {
-    var startDay = parseInt(startDate[0])
-        , startMonth = parseInt(startDate[1])
-        , startYear = parseInt(startDate[2])
-        , endDay = parseInt(endDate[0])
-        , endMonth = parseInt(endDate[1])
-        , endYear = parseInt(endDate[2])
-        , yearDiff = endYear - startYear
-        , monthDiff = (endMonth - startMonth) + (yearDiff * 12)
-        , dayDiff = endDay - startDay
-        , totalDays = 0;
-
-    var numberOfFullYears = yearDiff === 0 || yearDiff === 1 ? 0 : yearDiff - 1;
-    var numberOfFullMonths = endMonth < startMonth ? 12 - (startMonth - endMonth) : monthDiff;
-    if (monthDiff < 1) {
-        totalDays = dayDiff
-    } else {
-        totalDays += getNumberOfDaysFromStartAndEndMonths(startMonth, startDay, endDay);
-        totalDays += addFullMonths(startMonth, numberOfFullMonths, startYear);
-        totalDays += addFullYears(startYear, numberOfFullYears);
-    }
-    return totalDays;
-}
-
-var getNumberOfFullDaysBetweenDates = function (startDate, endDate) {
-    startDate = startDate.split("/");
-    endDate = endDate.split("/");
-    if (isStartDateBeforeEndDate(startDate, endDate)) {
-        var temp = startDate;
-        startDate = endDate;
-        endDate = temp;
-    }
-    var totalDays = calulateNumberOfFullDays(startDate, endDate);
-    if (totalDays != 0) { totalDays-- }
-    return totalDays;
-};
 
 function getNumberOfDaysInMonth(month) {
     return months[month - 1];
-};
+}
 
 function isStartDateBeforeEndDate(startDate, endDate) {
     return ((startDate[2] > endDate[2]) || (startDate[2] === endDate[2] && startDate[1] > endDate[1]) || (startDate[2] === endDate[2] && startDate[1] === endDate[1] && startDate[0] > endDate[0]));
-};
+}
 
 var isLeapYear = function (year) {
     return (year % 4 === 0 && year % 100 != 0) || (year % 4 === 0) && (year % 100 === 0 && year % 400 === 0);
